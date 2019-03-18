793b6ddaa36b90e73f769be5abce62085efac983
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-03-18 17:14:27 +0800

    Replace Facebook auth with GitHub and add filestore session storage (#50)
    * Replace Passport Facebook with Passport GitHub
    
    * Add FileStore session storage
    
    * Update README and add server start message

--- config.example.js
+++ config.example.js
@@ -1,12 +1,30 @@
 module.exports = {
-  repo: '/Users/yangshun/Developer/nusmods/',
-  app: '/Users/yangshun/Developer/nusmods/www',
-  stagingDir: '/Users/yangshun/Developer/nusmods/www/dist',
-  productionDir: '/Users/yangshun/Developer/nusmods.com',
+  // Location of the NUSMods repo root and the website root on the local filesystem
+  repo: `${process.env.HOME}/Developer/nusmods/`,
+  app: `${process.env.HOME}/Developer/nusmods/www`,
+
+  // Location of the NUSMods website files for staging and production on the local filesystem
+  stagingDir: `${process.env.HOME}/Developer/nusmods/www/dist`,
+  productionDir: `${process.env.HOME}/Developer/nusmods.com`,
+
+  // Where Launchpad will be served from
   host: 'http://localhost:3000',
+
+  // Generate a long, randomly generated string to encrypt login sessions
   sessionSecret: 'RANDOMLY GENERATED KEY',
+
+  // The Slack API token and the channel on which the updates will be posted
   slackBroadcastChannels: ['nusmods'],
   slackAPIToken: 'SLACK_TOKEN',
-  facebookAppID: 'APP_ID',
-  facebookAppSecret: 'APP_SECRET',
+
+  // GitHub app client ID and secret from https://github.com/organizations/nusmodifications/settings/applications
+  // and a whitelist of users allowed to access the app
+  githubClientId: 'CLIENT_ID',
+  githubClientSecret: 'CLIENT_SECRET',
+  githubUsernames: [
+    'ZhangYiJiang',
+    'taneliang',
+    'li-kai',
+    'jiholim',
+  ],
 };
--- server.js
+++ server.js
@@ -1,54 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
-const config = require('./config');
+const ensureLoggedIn = require('connect-ensure-login');
 
 const path = require('path');
 
 const dashboard = require('./routes/dashboard');
 const commits = require('./routes/commits');
-
-// Configure the Facebook strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(
-  new Strategy(
-    {
-      clientID: config.facebookAppID,
-      clientSecret: config.facebookAppSecret,
-      callbackURL: `${config.host}/login/facebook/return`,
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      // In this example, the user's Facebook profile is supplied as the user
-      // record.  In a production-quality application, the Facebook profile should
-      // be associated with a user record in the application's database, which
-      // allows for account linking and authentication with other identity
-      // providers.
-      return cb(null, profile);
-    },
-  ),
-);
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
-passport.serializeUser((user, cb) => {
-  cb(null, user);
-});
-
-passport.deserializeUser((obj, cb) => {
-  cb(null, obj);
-});
+const auth = require('./auth');
 
 // Create a new Express application.
 const app = express();
@@ -62,34 +19,15 @@ app.set('view engine', 'pug');
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(
-  require('express-session')({
-    secret: config.sessionSecret,
-    resave: true,
-    saveUninitialized: true,
-  }),
-);
+
+auth(app);
+
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/login/facebook', passport.authenticate('facebook'));
-
-app.get(
-  '/login/facebook/return',
-  passport.authenticate('facebook', { failureRedirect: '/' }),
-  (req, res) => {
-    res.redirect('/');
-  },
-);
 
 app.use('/', dashboard);
 app.use(
   '/commits',
-  require('connect-ensure-login').ensureLoggedIn('/'),
+  ensureLoggedIn.ensureLoggedIn('/'),
   (req, res, next) => {
     // Do not allow any actions if there is an ongoing build.
     if (commits.ongoingBuild) {
@@ -101,4 +39,7 @@ app.use(
   commits.router,
 );
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+console.log(`Server started on port ${port}`);
